0f7fe33f4a42a6c1b6684e208d50c07453c02bce
khanhdat111 <128824280+khanhdat111@users.noreply.github.com>
2024-06-14 18:45:26 +0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -2,13 +2,23 @@ from libs.data.hdr2nii import conver_hdr2nii
 
 import json
 
-hdrpath = "/kaggle/input/iseg19/iSeg-2019-Training/iSeg-2019-Training"
+# hdrpath = "/kaggle/input/iseg19/iSeg-2019-Training/iSeg-2019-Training"
+# niipath = "/kaggle/working/imagesTr"
+# labelpath = "/kaggle/working/labelsTr"
+# val_hdrpath = "/kaggle/input/iseg19/iSeg-2019-Validation"
+# val_niipath = "/kaggle/working/imagesTs"
+
+# conver_hdr2nii(hdrpath, niipath, labelpath, val_niipath, val_hdrpath)
+
+#For pure nii data
+training_datapath = "/kaggle/input/brats20-dataset-training-validation/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData"
+validation_datapath = "/kaggle/input/brats20-dataset-training-validation/BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData"
 niipath = "/kaggle/working/imagesTr"
 labelpath = "/kaggle/working/labelsTr"
-val_hdrpath = "/kaggle/input/iseg19/iSeg-2019-Validation"
 val_niipath = "/kaggle/working/imagesTs"
 
-conver_hdr2nii(hdrpath, niipath, labelpath, val_niipath, val_hdrpath)
+process_brats_files(training_datapath, validation_datapath, niipath, labelpath, val_niipath)
+
 with open("/kaggle/working/datalist.json") as f:
     datalist = json.load(f)
     
